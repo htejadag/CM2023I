d7edfe0cca4f21063553942af04486df2934eebc
Hernando Tejada <htejadag25@gmail.com>
2023-06-20 16:03:21 -0500

    Implementación de navegación básica entre menú principal y menú de controles básicos

--- app/src/main/java/com/example/cm2023i/MainActivity.java
+++ app/src/main/java/com/example/cm2023i/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cm2023i;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ControlesBasicos(View view)
+    {
+        Intent intent = new Intent(this, LayoutBasico.class);
+        startActivity(intent);
+    }
+
+    public void ControlesIntermedios(View view)
+    {
+
+    }
+
+    public void ControlesAvanzados(View view)
+    {
+
     }
 }
