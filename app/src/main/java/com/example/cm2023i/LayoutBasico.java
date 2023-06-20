abc56cd18fc9f60a660cf2adc02237449dc5bc92
Hernando Tejada <htejadag25@gmail.com>
2023-06-20 11:26:17 -0500

    Creación de layout básico con botones de ejemplos de textos

---
+++ app/src/main/java/com/example/cm2023i/LayoutBasico.java
@@ -0,0 +1,14 @@
+package com.example.cm2023i;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutBasico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_basico);
+    }
+}
