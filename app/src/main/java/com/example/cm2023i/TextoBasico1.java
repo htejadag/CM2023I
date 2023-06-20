13d6b3c81ce6e2188ab94cff765448957d8327ab
Hernando Tejada <htejadag25@gmail.com>
2023-06-20 15:40:21 -0500

    Mostrar toast con valores ingresados en cajas de texto

--- app/src/main/java/com/example/cm2023i/TextoBasico1.java
+++ app/src/main/java/com/example/cm2023i/TextoBasico1.java
@@ -3,12 +3,30 @@ package com.example.cm2023i;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class TextoBasico1 extends AppCompatActivity {
+
+    // Declarar los controles a utilizar
+    EditText txt1, txt2, txt3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_texto_basico1);
+
+        // Setear los controles a sus respectivas variables
+        txt1 = findViewById(R.id.txt1_1_1);
+        txt2 = findViewById(R.id.txt1_1_2);
+        txt3 = findViewById(R.id.txt1_1_3);
+    }
+
+
+    public void Mostrar(View view)
+    {
+        String msg = txt1.getText() + " - " + txt2.getText() + " - " + txt3.getText();
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
